e33f42d403aa2b1479774fa91851b4fc292b8312
elenal63 <91587602+elenal63@users.noreply.github.com>
2022-12-07 18:19:42 -0700

    fixed errors

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     static void Main(string[] args)
     {
-        
+        //Person Person1 = new Person(68, 120, 20, "f", "la");
+        Console.WriteLine(HealthActions.findCategory());
     }
 }
